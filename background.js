8b58db34b16d69c483d1502697cdf554d7049e1c
LukasDelilez <lukas.delilez@gmx.de>
2024-09-12 07:17:41 +0200

    Removed unnescessary clipboard message sending to popup Changed context menu items context to 'page'

--- background.js
+++ background.js
@@ -3,28 +3,18 @@ const runtime = typeof chrome !== 'undefined' ? chrome : browser;
 runtime.contextMenus.create({
     id: "copyHighlights",
     title: "Copy All Highlights",
-    contexts: ["all"]
+    contexts: ["page"]
 });
 
 runtime.contextMenus.create({
     id: "removeHighlights",
     title: "Remove All Highlights",
-    contexts: ["all"]
+    contexts: ["page"]
 });
 
 runtime.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "copyHighlights") {
-        runtime.tabs.sendMessage(tab.id, {action: "copyHighlights"}, response => {
-            if (response && response.text) {
-                if (typeof chrome !== 'undefined') {
-                    navigator.clipboard.writeText(response.text).then(() => {
-                        console.log('Highlighted text copied to clipboard');
-                    });
-                } else {
-                    runtime.tabs.sendMessage(tab.id, { action: "copyToClipboard", text: response.text });
-                }
-            }
-        });
+        runtime.tabs.sendMessage(tab.id, {action: "copyHighlights"});
     }
 
     if(info.menuItemId === "removeHighlights") {
